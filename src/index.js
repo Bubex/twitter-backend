c85e5d9f316357527bdce5758a2865529385cb05
Bubex <marlon2mrt@gmail.com>
2020-05-14 00:48:17 -0300

    configure heroku port

--- src/index.js
+++ src/index.js
@@ -26,4 +26,4 @@ app.use(express.json());
 app.use('/images', express.static(path.resolve('__dirname', '..', 'tmp', 'uploads')));
 app.use(routes);
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
